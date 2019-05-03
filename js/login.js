ce95039c905d5ef260d4bd810f71150e29522152
syafiq shamsuddin <syafiq555@github.com>
2019-05-04 05:09:37 +0800

    test login api

---
+++ js/login.js
@@ -0,0 +1,46 @@
+$(document).ready(() => {
+  const api_url = 'https://car-rental-system-api.herokuapp.com'
+
+  $('form#loginForm').submit(async event => {
+    event.preventDefault()
+    const button = (event.target).querySelector('button[type=submit]')
+
+    const { password, username } = event.target.elements
+    const valid = validateForm(username.value, password.value)
+    if (!valid.valid)
+      return alert(valid.msg)
+
+    $(button).addClass('disabled')
+
+    const res = await login(username.value, password.value)
+    if (res.status === -1) return alert('User not found')
+    if (res.status === 0) return alert('Wrong password')
+
+    return alert(`Login successful token: ${res.token}`)
+  })
+
+  const validateForm = (username, password) => {
+    if (username.length < 5 || password.length < 6) {
+      return {
+        msg: 'username length must be bigger than 4 and password length must be bigger than 5',
+        valid: false
+      }
+    }
+
+    return { valid: true }
+  }
+
+  const login = async (username, password) => {
+    const res = await fetch(`${api_url}/auth`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({ username, password })
+    })
+
+    const valid = await res.json()
+    return valid
+  }
+})
