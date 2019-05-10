5f5b02881404ff1e40dc67aabfe16a813f7dbcb5
Unknown <Shyuffix555@gmail.com>
2019-05-11 05:11:59 +0800

    set session for user id

--- js/login.js
+++ js/login.js
@@ -35,6 +35,7 @@ $(document).ready(() => {
 
     sessionStorage.role = res.role
     sessionStorage.token = res.token
+    sessionStorage.user_id = res.id
     Swal.fire('Login Successful', `Welcome back, ${username.value}`, 'success')
     setTimeout(() => window.location.href = "home.html#home", 2000)
 
